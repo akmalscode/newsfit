8a77ae2ae037c9d932b1a43ffb9f17c210603f6b
Md Akmal Hossain <akmal2080@gmail.com>
2019-07-25 17:39:22 -0400

    variable

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const Article = mongoose.model('Article', articleSchema);
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, function() {
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, function(err) {
     if (err) {
         console.log(err)
     }
