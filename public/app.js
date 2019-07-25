1fba7fa24a982f7df0e3e25669599f036a5175f4
Md Akmal Hossain <akmal2080@gmail.com>
2019-07-25 17:30:42 -0400

    add css

--- public/app.js
+++ public/app.js
@@ -1,19 +1,30 @@
 console.log('script loaded')
 
+
+function attachSaveListener() {
+    $('.save-article').on('click', function(e) {
+        $.post('/save-article', {
+            link: e.target.value
+        })
+    })
+}
+
 $('#scrape-button').on('click', function() {
-    console.log('button clicked')
     $.get('/scrape').then(function(res) {
-        console.log(res)
         res.forEach(function(article) {
 
             $('#articles').append(`
         
             <div>
-                <h1>${article}</h1>
+                <h2>${article.header}</h2>
+                <hr />
+                <button class="save-article" value="${article.link}">Save Article</button>
             </div>
         
         `)
 
         })
+
+        attachSaveListener();
     })
 })
--- server.js
+++ server.js
@@ -3,25 +3,37 @@ const mongoose = require('mongoose');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const expresshandlebars = require('express-handlebars');
+const bodyParser = require('body-parser')
+
+//var app = express();
 
 
 
+const articleSchema = new mongoose.Schema({
+    link: String
+});
 
-
-
-
+const Article = mongoose.model('Article', articleSchema);
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-const app = express();
+var app = express();
+
+//using css file
+//app.use('public', express.static('public'));
+
+
 
 app.engine('handlebars', expresshandlebars());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
 
@@ -32,23 +44,39 @@ app.get('/', function(req, res) {
 
 
 app.get('/scrape', function(req, res) {
-
-
     axios.get('https://www.nytimes.com/section/politics').then(function(response) {
 
         const articles = [];
         const $ = cheerio.load(response.data)
 
         $('.e1xfvim30').map(function() {
-            let article = $(this).contents().text();
-            articles.push(article);
-            console.log(article)
+            let header = $(this).contents().text();
+            let link = $(this).closest('a').attr('href');
+
+            articles.push({
+                header,
+                link
+            });
         })
 
         return res.json(articles)
 
     })
-
 })
 
+app.post('/save-article', function(req, res) {
+    Article.create({
+        link: req.body.link
+    })
+
+    res.sendStatus(200)
+})
+
+app.get('/articles', function(req, res) {
+    const articles = Article.find();
+
+    res.render('articles', {
+        articles
+    });
+})
 app.listen(4000);
